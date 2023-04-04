3796345197f4b19da091b9d882f9d4ff68b1923c
Aziz Mahfoudh <mohamedaziz.mahfoudh@eniso.u-sousse.tn>
2023-04-04 03:09:17 +0200

    Added login User interface

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import LoginForm from "../component/login/login-form";
+import { Center, Container, Flex } from "@chakra-ui/react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Flex
+      width={"100vw"}
+      height={"100vh"}
+      alignContent={"center"}
+      justifyContent={"center"}>
+      <Center>
+        <LoginForm></LoginForm>
+      </Center>
+    </Flex>
+  );
 };
 
 export default Login;
